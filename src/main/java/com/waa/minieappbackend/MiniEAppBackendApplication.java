adc7258a6db7cc58d612bea2fc98ff98f1efbca9
Sagar <worthless.dumboo@gmail.com>
2022-04-16 17:02:11 -0500

    Skeleton

---
+++ src/main/java/com/waa/minieappbackend/MiniEAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.waa.minieappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waa/minieappbackend/MiniEAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waa.minieappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
