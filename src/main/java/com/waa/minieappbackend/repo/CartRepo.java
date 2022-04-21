07c7bc7f19d5c27def2a93b0e7ad9516d5c11d77
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-20 21:38:48 -0500

    fixed typo

--- src/main/java/com/waa/minieappbackend/repo/CartRepo.java
+++ src/main/java/com/waa/minieappbackend/repo/CartRepo.java
@@ -1,13 +1,13 @@
 package com.waa.minieappbackend.repo;
 
 import com.waa.minieappbackend.domain.Cart;
-import com.waa.minieappbackend.domain.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
+@Repository
 public interface CartRepo extends CrudRepository<Cart, Long> {
 
-    List<Product> find();
+//    List<Product> find();
 
 }
