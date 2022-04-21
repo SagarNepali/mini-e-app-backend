bc53381663252352f4ecf37217af32def84b448f
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-20 21:25:37 -0500

    Added security

---
+++ src/main/java/com/waa/minieappbackend/service/JwtUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.waa.minieappbackend.service;
+
+import com.waaAssig.Assignment.dao.UserDao;
+import com.waaAssig.Assignment.model.DAOUser;
+import com.waaAssig.Assignment.model.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired @Lazy
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		DAOUser user = userDao.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+	
+	public DAOUser save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return userDao.save(newUser);
+	}
+
+}
